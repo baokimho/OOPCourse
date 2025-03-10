68c66fc32a20c6b43c664c831eb8530254f7b24d
baokimho <kim.ho@edu.turkuamk.fi>
2025-03-11 00:54:41 +0200

    ad

---
+++ ex5/task1.py
@@ -0,0 +1,13 @@
+class City:
+    postcodes = {
+        "Helsinki": "00100",
+        "Turku": "20100",
+        "Tampere": "33100",
+        "Salo": "24100",
+        "Oulu": "90100",
+        "Jyväskylä": "40100"
+    }
+
+    def __init__(self, nimi: str, population: int):
+        self.__nimi = nimi
+        self.__population = population
