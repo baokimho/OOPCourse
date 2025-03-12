a473e483d19352311c4f5c757b4ebed01da33c82
baokimho <kim.ho@edu.turkuamk.fi>
2025-03-12 10:20:08 +0200

    task 3 part 1

--- ex5/task3.py
+++ ex5/task3.py
@@ -1,2 +1,19 @@
 class Item:
-    def __init__(self, )
+    def __init__(self, name, weight):
+        self.__name = name
+        self.__weight = weight
+    
+    def name(self):
+        return self.__name
+    
+    def weight(self):
+        return self.__weight
+
+    def __str__(self):
+        return f"{self.__name} ({self.__weight})"
+book = Item("ABC Book", 200)   
+phone = Item("Nokia 3210", 100) 
+print("Name of the book:", book.name())   
+print("Weight of the book:", book.weight())   
+print("Book:", book) 
+print("Phone:", phone) 
