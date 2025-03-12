c5c8935b6c79e890445d8a9ec8728e943d9fd128
baokimho <kim.ho@edu.turkuamk.fi>
2025-03-12 23:21:40 +0200

    task completed

--- ex5/task3.py
+++ ex5/task3.py
@@ -20,31 +20,59 @@ phone = Item("Nokia 3210", 100)
 # print("Book:", book) 
 # print("Phone:", phone) 
 
-# Part 2,3,4
+# Part 2,3,4,5
 
 class Suitcase:
     def __init__(self,max_weight):
         self.max_weight = max_weight
-        self.current_weight = 0
-        self.item_store = set()
+        self.items = []  
 
     def add_item(self, item):
-        if item.weight + self.current_weight <= self.max_weight :
-            self.current_weight += item.weight
-            self.item_store.add(str(item))
+        current_weight = self.weight()
+        if item.weight + current_weight <= self.max_weight:
+            self.items.append(item)  
 
     def print_items(self):
-        print (", ".join(self.item_store))
+        item_strings = [str(item) for item in self.items]
+        print(", ".join(item_strings))
     
     def weight(self):
-        return self.current_weight
+        return sum(item.weight for item in self.items)
+
+    def heaviest_item(self):
+        if not self.items:
+            return None
+        return max(self.items, key=lambda item: item.weight)  
     
     def __str__(self):
-        if len(self.item_store) == 1 :
-            return f"{len(self.item_store)} item ({self.current_weight} g)"
+        if len(self.items) == 1 :
+            return f"{len(self.items)} item ({self.weight()} g)"
         else:
-            return f"{len(self.item_store)} items ({self.current_weight} g)"
+            return f"{len(self.items)} items ({self.weight()} g)"
+
+class CargoHold:
+    def __init__(self, maximum_weight):
+        self.maximum_weight = maximum_weight* 1000
+        self.items = []  
     
+    def add_suitcase(self, suitcase):
+        if suitcase.weight() + self.weight() <= self.maximum_weight:
+            self.items.append(suitcase)
+    
+    def weight(self):
+        return sum(item.weight() for item in self.items) 
+       
+    def print_items(self):
+        for suitcase in self.items:
+            for item in suitcase.items:
+                print(item)
+    
+    def __str__(self):
+        remaining_space = self.maximum_weight - self.weight()
+        if len(self.items) == 1:
+            return f"{len(self.items)} suitcase, space for {remaining_space/1000} kg"
+        else:
+            return f"{len(self.items)} suitcases, space for {remaining_space/1000} kg"
 
 # book = Item("ABC Book", 200)   
 # phone = Item("Nokia 3210", 100) 
@@ -58,14 +86,54 @@ class Suitcase:
 # suitcase.add_item(brick)
 # print(suitcase)
 
+# book = Item("ABC Book", 200)   
+# phone = Item("Nokia 3210", 100) 
+# brick = Item("Brick", 400 ) 
+# suitcase = Suitcase(1000)   
+# suitcase.add_item(book) 
+# suitcase.add_item(phone)
+# suitcase.add_item(brick)
+# print("The suitcase contains the following items:") 
+# suitcase.print_items() 
+# combined_weight = suitcase.weight() 
+# print(f"Combined weight: {combined_weight} g") 
+
+# book = Item("ABC Book", 200)   
+# phone = Item("Nokia 3210", 100) 
+# brick = Item("Brick", 400 ) 
+# suitcase = Suitcase(1000)   
+# suitcase.add_item(book) 
+# suitcase.add_item(phone)
+# suitcase.add_item(brick)
+# heaviest = suitcase.heaviest_item() 
+# print(f"The heaviest item: {heaviest}") 
+
+# cargo_hold = CargoHold(100) 
+# print(cargo_hold)
+# book = Item("ABC Book", 200)   
+# phone = Item("Nokia 3210", 100) 
+# brick = Item("Brick", 400 ) 
+# adas_suitcase = Suitcase(1000) 
+# adas_suitcase.add_item(book) 
+# adas_suitcase.add_item(phone)
+# peters_suitcase = Suitcase(1000)
+# peters_suitcase.add_item(brick)
+# cargo_hold.add_suitcase(adas_suitcase) 
+# print(cargo_hold) 
+# cargo_hold.add_suitcase(peters_suitcase)
+# print(cargo_hold)
+
 book = Item("ABC Book", 200)   
 phone = Item("Nokia 3210", 100) 
 brick = Item("Brick", 400 ) 
-suitcase = Suitcase(1000)   
-suitcase.add_item(book) 
-suitcase.add_item(phone)
-suitcase.add_item(brick)
-print("The suitcase contains the following items:") 
-suitcase.print_items() 
-combined_weight = suitcase.weight() 
-print(f"Combined weight: {combined_weight} g") 
+adas_suitcase = Suitcase(1000) 
+adas_suitcase.add_item(book) 
+adas_suitcase.add_item(phone)
+peters_suitcase = Suitcase(1000)
+peters_suitcase.add_item(brick)
+cargo_hold = CargoHold(100) 
+cargo_hold.add_suitcase(adas_suitcase) 
+cargo_hold.add_suitcase(peters_suitcase)
+print("The suitcases in the cargo hold contain the following items:") 
+cargo_hold.print_items()
+
