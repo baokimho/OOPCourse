a4f319b6a743aeb02e694b43442b94e6f3a00599
baokimho <kim.ho@edu.turkuamk.fi>
2025-04-03 15:37:27 +0300

    task 4

---
+++ ex8/task4.py
@@ -0,0 +1,8 @@
+def calculate_area(radius):
+    return 3.14 * radius * radius
+
+def calculate_perimeter(radius):
+    return 2 * 3.14 * radius
+
+print(calculate_area(23))
+print(calculate_perimeter(21))
